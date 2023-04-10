e844b36c27cd7ad1426c567bd5d42d8ea2eb54cd
Muzira Kellia <kelliamuzira@gmail.com>
2023-04-10 13:06:48 -0700

    updated

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absoute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of int
+ */ 
+int _abs(int n)
+{
+	if(n >= 0)
+	{
+		return(n);
+	}
+	return(-n);
+}
